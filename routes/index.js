8575bb923c67c2181441dcf1298119cf37a98b81
pateldk2507 <pateldk2507@gmail.com>
2025-12-03 20:06:44 -0500

    welcome message for the Store Management API

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const apiV1Routes = require('./api/v1');
 
+router.get('/', (req, res) => {
+  res.send('Welcome to the Store Management API');
+});
+
 router.use('/api/v1', apiV1Routes);
 
 module.exports = router;

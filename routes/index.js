5da55185f68c1d0c23688f39c2b7560af6143c1d
pateldk2507 <pateldk2507@gmail.com>
2025-12-08 21:39:31 -0500

    color added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const apiV1Routes = require('./api/v1');
 
 router.get('/', (req, res) => {
-  res.send('<center><h1>Welcome to Ci/CD Store Management API </h1></center>');
+  res.send('<center><h1 style="color:blue;">Welcome to Ci/CD Store Management API </h1></center>');
 });
 
 router.use('/api/v1', apiV1Routes);

2d09b8370320bcc5fc79cfe58944ca3b02b53adf
pateldk2507 <pateldk2507@gmail.com>
2025-12-14 16:24:57 -0500

    message changed on home screen.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p> welcom to Home Page ! </p>
+      <p> welcom to Home Page !, Change Made by Divya </p>
     </div>
   );
 };

12cff3a23461fb2e6a3dc5dfa79d21d14addb77c
pateldk2507 <pateldk2507@gmail.com>
2025-12-15 20:30:53 -0500

    404 updated

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,10 +1,16 @@
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px', color: 'white',  padding: '20px', backgroundColor: 'red'}}>
-      <h1>!!!!!!!!!!!!!!Not Found!!!!!!!!!!!!!!!!!</h1>
-      <p>!!!!!!!!!!!!!!!!!This!!!!!!!!!!!!!!!!!!!!</p>
-      <p>!!!!!!!!!!!!!!!location!!!!!!!!!!!!!!!!!!</p> 
-      <p>!!!!!!!!!!!!!!is not exist!!!!!!!!!!!!!!!</p>
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "50px",
+        color: "white",
+        padding: "20px",
+        backgroundColor: "red",
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
     </div>
   );
 };

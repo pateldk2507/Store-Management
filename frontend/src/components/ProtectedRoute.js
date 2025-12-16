2d338b6422451ef2b1635f5311ce54f8fe52f77b
pateldk2507 <pateldk2507@gmail.com>
2025-12-16 18:23:24 -0500

    new theme added

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children, isAuthenticated }) => {
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;

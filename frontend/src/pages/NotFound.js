0b200001e8a0711507e49eea99c9f12aeabb50dd
pateldk2507 <pateldk2507@gmail.com>
2025-12-15 19:39:00 -0500

    color changed

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px', color: 'red', backgroundColor: 'black', padding: '20px', backgroundColor: 'lightgrey'}}>
+    <div style={{ textAlign: 'center', marginTop: '50px', color: 'white',  padding: '20px', backgroundColor: 'red'}}>
       <h1>!!!!!!!!!!!!!!Not Found!!!!!!!!!!!!!!!!!</h1>
       <p>!!!!!!!!!!!!!!!!!This!!!!!!!!!!!!!!!!!!!!</p>
       <p>!!!!!!!!!!!!!!!location!!!!!!!!!!!!!!!!!!</p> 
